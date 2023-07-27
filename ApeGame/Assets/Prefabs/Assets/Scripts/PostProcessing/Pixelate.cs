e3c4fd59c7f6a4682288e458b43477dd6f07ddfb
bgeoffreys <132320716+bgeoffreys@users.noreply.github.com>
2023-07-27 15:06:31 -0700

    sKIBIDI

---
+++ ApeGame/Assets/Prefabs/Assets/Scripts/PostProcessing/Pixelate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+
+namespace Oxeren.PostProcessing
+{
+
+    [Serializable, PostProcess(typeof(PixelateRenderer), PostProcessEvent.AfterStack, "Custom/Pixelate")]
+    public sealed class Pixelate : PostProcessEffectSettings
+    {
+
+        [Range(1, 16)]
+        public IntParameter DownscaleIterations = new IntParameter { value = 4 };
+        public IntParameter ColorReduction = new IntParameter { value = 16 };
+
+        public override bool IsEnabledAndSupported(PostProcessRenderContext context)
+        {
+            return enabled;
+        }
+    }
+
+    public sealed class PixelateRenderer : PostProcessEffectRenderer<Pixelate>
+    {
+
+        PropertySheet sheet;
+
+        public override void Render(PostProcessRenderContext context)
+        {
+
+            if (sheet == null)
+                sheet = context.propertySheets.Get(Shader.Find("Custom/Pixelate"));
+
+            sheet.properties.SetFloat("_ColorReductionIterations", settings.ColorReduction.value);
+            int width = context.width / settings.DownscaleIterations;
+            int height = context.height / settings.DownscaleIterations;
+            var downsampled = RenderTexture.GetTemporary(width, height, 0, context.sourceFormat);
+            downsampled.filterMode = FilterMode.Point;
+            context.command.BlitFullscreenTriangle(context.source, downsampled);
+            context.command.BlitFullscreenTriangle(downsampled, context.destination, sheet, 0);
+            RenderTexture.ReleaseTemporary(downsampled);
+        }
+    }
+
+}
---
+++ ApeGame/Assets/Prefabs/Assets/Scripts/PostProcessing/Pixelate.shader
@@ -0,0 +1,31 @@
+﻿Shader "Custom/Pixelate"
+{
+	HLSLINCLUDE
+
+	#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+	TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+	float _ColorReductionIterations;
+
+	ENDHLSL
+	
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+		Pass //0 Applying Box filtering
+		{
+			HLSLPROGRAM
+
+			#pragma vertex VertDefault
+			#pragma fragment Frag
+
+			float4 Frag(VaryingsDefault i) : SV_Target
+			{
+				return round( SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord) * _ColorReductionIterations) / _ColorReductionIterations;
+			}
+			ENDHLSL
+		}
+	}
+
+}
