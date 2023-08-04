ce8d343ae05cf9c72d7a1e73bc4c2455aca2afb4
jfallz <45546400+jfallz@users.noreply.github.com>
2023-08-04 08:47:27 -0700

    stuff

--- ApeGame/Assets/Scripts/GameManager.cs
+++ ApeGame/Assets/Scripts/GameManager.cs
@@ -7,18 +7,23 @@ public class GameManager : MonoBehaviour
 {
     public CannonFire scriptRef;
     public int distanceTraveled = 0;
-    public int maxDistance = 0;
+    // ----
+    public float maxDistance = 0f;
+    public float maxSpeed = 0f;
+    public float maxAltitude = 0f;
+    // ----
     private float speed = 0f;
     private int startingDistance;
     [SerializeField] public float currency;
     public GameObject prefabToInstantiate;
+    public GameObject playerMenu;
     public bool restart = false; // if true, will restart the player and then reset bool to false
     public float TimeT = 0f;
     private bool inFlight = false; 
     private bool launched = false;
     public GameObject speedometer;
-    public float min = 311f;
-    public float max = 50f;
+    public float min = 317f;
+    public float max = 45f;
 
     // public void Start() {
     //     scriptRef = GameObject.Find("Rotation").GetComponent<CannonFire>();
@@ -38,12 +43,14 @@ public class GameManager : MonoBehaviour
                 if(rb.velocity.magnitude < 1) 
                     Death();
             }
-            float angle = min - rb.velocity.magnitude / 2f;
 
-            speedometer.transform.eulerAngles = new Vector3(speedometer.transform.eulerAngles.x,
-                                                            speedometer.transform.eulerAngles.y,
-                                                            angle);
-            speed = rb.velocity.magnitude;
+            // speedometer
+            // float angle = min - (rb.velocity.magnitude / 100f);
+
+            // speedometer.transform.eulerAngles = new Vector3(speedometer.transform.eulerAngles.x,
+            //                                                 speedometer.transform.eulerAngles.y,
+            //                                                 speedometer.transform.eulerAngles.z - angle);
+            // speed = rb.velocity.magnitude;
             statTrack();
         }
     }
@@ -55,36 +62,26 @@ public class GameManager : MonoBehaviour
         d.text = "DISTANCE: " + distanceTraveled;
     }
     public void Death() {
-        GameObject targetObject = GameObject.FindWithTag("Cart");
-        Vector3 targetPosition = targetObject.transform.position;
-        Quaternion targetRotation = targetObject.transform.rotation;
-        Destroy(targetObject);
-        Destroy(GameObject.FindWithTag("Player"));
-
-            // Instantiate the prefab at the target position with the same rotation.
-        GameObject newObject = Instantiate(prefabToInstantiate, targetPosition, targetRotation);
-
         currency = distanceTraveled * .25f;
         TextMeshProUGUI c = GameObject.Find("CURRENCY").GetComponent<TextMeshProUGUI>();
         c.text = "CURRENCY: " + (int)currency;
-        distanceTraveled = 0;
-        launched = false;
-
+        playerMenu.SetActive(true);
     }
 
     public void ShopMenu() {
     }
 
     public void Restart() {
-            // // tell gamemanager we're no longer dead
-            // dead = false;
+        playerMenu.SetActive(false);
+        GameObject targetObject = GameObject.FindWithTag("Cart");
+        Vector3 targetPosition = targetObject.transform.position;
+        Quaternion targetRotation = targetObject.transform.rotation;
+        Destroy(targetObject);
+        Destroy(GameObject.FindWithTag("Player"));
+            // Instantiate the prefab at the target position with the same rotation.
+        GameObject newObject = Instantiate(prefabToInstantiate, targetPosition, targetRotation);
+        launched = false;
+        distanceTraveled = 0;
 
-            // Destroy(GameObject.FindWithTag("CartCracked"));
-            // Destroy(GameObject.FindWithTag("GameController"));
-            // Destroy(GameObject.FindWithTag("Cart"));
-            // Destroy(GameObject.Find("Ragdoll"));
-            // GameObject player = Instantiate(prefabToInstantiate, spawnPoint.position, Quaternion.identity); // instantiate new cannon
-            // Shop.GetComponent<ShopManager>().PerformUpgrades(player);
-            // restart = false;
     }
 }
