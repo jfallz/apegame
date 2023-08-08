19add62f243b5a4afbcaf719b0b329bb692d4c04
jfallz <45546400+jfallz@users.noreply.github.com>
2023-08-07 18:59:31 -0700

    oksir

--- ApeGame/Assets/Prefabs/MeshMaskScriptUI.cs
+++ ApeGame/Assets/Prefabs/MeshMaskScriptUI.cs
@@ -5,16 +5,30 @@ using UnityEngine.UI;
 
 public class MeshMaskScriptUI : MaskableGraphic
 {
+    float count = 0;
+    bool rising = true;
   
     protected override void OnPopulateMesh(VertexHelper vertexHelper)
     {
         vertexHelper.Clear();
 
+        if((count * 5f) > 1025) {
+            rising = false;
+        } else if((count * 5f) < 200) {
+            rising = true;
+        }
+
         Vector3 vec_00 = new Vector3(0, 0);
         Vector3 vec_01 = new Vector3(0, 50);
-        Vector3 vec_10 = new Vector3(50 + Time.realtimeSinceStartup * 40f, 0);
-        Vector3 vec_11 = new Vector3(50 + Time.realtimeSinceStartup * 40f, 50);
+        Vector3 vec_10 = new Vector3(count * 5f, 0);
+        Vector3 vec_11 = new Vector3(count * 5f, 50);
 
+        if(rising)
+            ++count;
+        else
+            --count;
+        
+        print(vec_10);
         vertexHelper.AddUIVertexQuad(new UIVertex[]
         {
             new UIVertex { position = vec_00, color = Color.green },
@@ -22,6 +36,7 @@ public class MeshMaskScriptUI : MaskableGraphic
             new UIVertex { position = vec_11, color = Color.green },
             new UIVertex { position = vec_10, color = Color.green },
         });
+
     }
 
     private void Update()
